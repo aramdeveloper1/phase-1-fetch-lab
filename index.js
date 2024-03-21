7765a21b2dcfaa0e1c9507f135769b9cbc3f2732
unknown <aramdeveloper1@gmail.com>
2024-03-21 19:19:44 +0300

    commit

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json());
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;  // Safer alternative to innerHTML
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks()
+    .then(books => renderBooks(books));
 });
